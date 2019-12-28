79b98db5aadc9ca6ce4e7f655d03425e5c49d5b4
putuAdiSudarmawan <dokumensundel@gmail.com>
2019-12-28 15:23:27 +0800

    tugas akhir lokalisasi bahasa

--- app/src/main/java/com/example/profile/MainActivity.java
+++ app/src/main/java/com/example/profile/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t= (TextView) findViewById(R.id.myPict);
-        Typeface myCustomFont=Typeface.createFromAsset(getAssets(),"fonts/Lora-Regular.ttf");
-        t.setTypeface(myCustomFont);
+        ImageButton setting =(ImageButton) findViewById(R.id.setting);
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
+        });
     }
 }
